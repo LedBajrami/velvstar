c9157caf76a0aa58d1fe4241df9509c729c7296a
LedBajrami <108076169+LedBajrami@users.noreply.github.com>
2023-04-03 17:06:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -94,9 +94,25 @@ document.addEventListener('click', (e)=> {
 const popupBtn = document.querySelector("#viewreport")
 const popupBtn2 = document.querySelector("#viewreport2")
 const popupBtn3 = document.querySelector("#viewreport3")
+const popupBtn4 = document.querySelector("#viewreport4")
+const popupBtn5 = document.querySelector("#viewreport5")
+const popupBtn6 = document.querySelector("#viewreport6")
+const popupBtn7= document.querySelector("#viewreport7")
+const popupBtn8 = document.querySelector("#viewreport8")
+const popupBtn9 = document.querySelector("#viewreport9")
+const popupBtn10 = document.querySelector("#viewreport10")
+
 const popUpClient = document.querySelector("#pop-up-client")
 const popUpClient2 = document.querySelector("#pop-up-client2")
 const popUpClient3 = document.querySelector("#pop-up-client3")
+const popUpClient4 = document.querySelector("#pop-up-client4")
+const popUpClient5 = document.querySelector("#pop-up-client5")
+const popUpClient6 = document.querySelector("#pop-up-client6")
+const popUpClient7 = document.querySelector("#pop-up-client7")
+const popUpClient8 = document.querySelector("#pop-up-client8")
+const popUpClient9 = document.querySelector("#pop-up-client9")
+const popUpClient10 = document.querySelector("#pop-up-client10")
+
 const overlay = document.querySelector('.overlay')
 const closePopUpBtn =  Array.from(document.querySelectorAll("#close-popup"))
 const thankYouPopUp = document.querySelector('#thankyou-popup')
@@ -106,18 +122,25 @@ const closeThankYouPopUpBtn = document.querySelector("#close-thankyou-popup")
 
 
 
-popupBtn.addEventListener('click', ()=> {
-  popUpClient.style.display = 'block'
-  overlay.style.display = 'block'
-})
-popupBtn2.addEventListener('click', ()=> {
-  popUpClient2.style.display = 'block'
-  overlay.style.display = 'block'
-})
-popupBtn3.addEventListener('click', ()=> {
-  popUpClient3.style.display = 'block'
-  overlay.style.display = 'block'
-})
+
+
+function openPopUp(button, popUp) {
+  button.addEventListener('click', ()=> {
+    popUp.style.display = 'block'
+    overlay.style.display = 'block'
+  })
+}
+
+openPopUp(popupBtn, popUpClient)
+openPopUp(popupBtn2, popUpClient2)
+openPopUp(popupBtn3, popUpClient3)
+openPopUp(popupBtn4, popUpClient4)
+openPopUp(popupBtn5, popUpClient5)
+openPopUp(popupBtn6, popUpClient6)
+openPopUp(popupBtn7, popUpClient7)
+openPopUp(popupBtn8, popUpClient8)
+openPopUp(popupBtn9, popUpClient9)
+openPopUp(popupBtn10, popUpClient10)
 
 
 overlay.addEventListener('click', closePopUp)
@@ -127,18 +150,28 @@ closePopUpBtn.forEach(btn => {
 })
 
 
-thankYouPopUpBtn.addEventListener('click', openThankYouPopUp)
-closeThankYouPopUpBtn.addEventListener('click', closeThankYouPopUp)
-overlay.addEventListener('click', closeThankYouPopUp)
-
-
 function closePopUp() {
   popUpClient.style.display = 'none'
   popUpClient2.style.display = 'none'
   popUpClient3.style.display = 'none'
+  popUpClient4.style.display = 'none'
+  popUpClient5.style.display = 'none'
+  popUpClient6.style.display = 'none'
+  popUpClient7.style.display = 'none'
+  popUpClient8.style.display = 'none'
+  popUpClient9.style.display = 'none'
+  popUpClient10.style.display = 'none'
   thankYouPopUp.style.display = 'none'
   overlay.style.display = 'none'
 }
+
+
+
+
+thankYouPopUpBtn.addEventListener('click', openThankYouPopUp)
+closeThankYouPopUpBtn.addEventListener('click', closeThankYouPopUp)
+overlay.addEventListener('click', closeThankYouPopUp)
+
 
 
 
